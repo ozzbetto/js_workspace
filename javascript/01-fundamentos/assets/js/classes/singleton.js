f18d05a63312068782c6063fc1c2af14edf82743
BettoR <franrod1082@gmail.com>
2023-08-22 07:15:57 -0400

    Cambios menores

--- javascript/01-fundamentos/assets/js/classes/singleton.js
+++ javascript/01-fundamentos/assets/js/classes/singleton.js
@@ -19,5 +19,5 @@ const instancia2 = new Singleton('Metallica');
 const instancia3 = new Singleton('Sepultura');
 
 console.log(`Nombre en la instancia1 es: ${ instancia1.nombre }`);
-console.log(`Nombre en la instancia1 es: ${ instancia2.nombre }`);
-console.log(`Nombre en la instancia1 es: ${ instancia3.nombre }`);
+console.log(`Nombre en la instancia2 es: ${ instancia2.nombre }`);
+console.log(`Nombre en la instancia3 es: ${ instancia3.nombre }`);
