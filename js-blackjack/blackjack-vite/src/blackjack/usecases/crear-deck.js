be128ba71811cf40be81bfb72b9f3fbc80a467d3
Betto Rodriguez <segovia.al@gmail.com>
2023-08-26 14:24:08 -0400

    Se agregan JSDoc a las funciones

--- js-blackjack/blackjack-vite/src/blackjack/usecases/crear-deck.js
+++ js-blackjack/blackjack-vite/src/blackjack/usecases/crear-deck.js
@@ -1,6 +1,15 @@
 import _ from 'underscore';
 
+/**
+ * Esta función crea un nuevo deck
+ * @param {array<string>} tiposDeCarta Ejemplo: ['C','D', 'H', 'S'];
+ * @param {array<string>} tiposEspeciales Ejemplo: ['A', 'J', 'Q', 'K']
+ * @returns {array<string>} retorna un nuevo deck de cartas
+ */
 export const crearDeck = (tiposDeCarta, tiposEspeciales) => {
+
+    if ( !tiposDeCarta ) throw new Error('Tipos de carta es obligatorio');
+    if ( tiposDeCarta.lenght === 0 ) throw new Error('TiposDeCarta tiene que ser un arreglo de string')
     
     let deck = [];
     for( let i = 2; i <= 10; i++ ) {
